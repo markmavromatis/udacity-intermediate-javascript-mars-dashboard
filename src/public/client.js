89a6409799fcde94e459b212fa4cf58095a541b9
Mark Mavromatis <markmavromatis@Marks-2020-Macbook-Pro-13.local>
2020-09-15 00:04:09 -0400

    Removed unused starter code functions

--- src/public/client.js
+++ src/public/client.js
@@ -216,71 +216,7 @@ window.addEventListener('load', () => {
 
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
-
-    return `
-        <h1>Hello!</h1>
-    `
-}
-
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-
-    // This image fires twice. Prevent it from referring to an invalid image URL if no apod exists.
-    if (!apod) {
-        return;
-    }
-
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-// 
-    // console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
-    console.log(JSON.stringify(apod));
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
-}
 
 // ------------------------------------------------------  API CALLS
 
-function getFormattedDate() {
-    const todayDate = new Date();
-    const year = todayDate.getFullYear();
-    const monthAsNumber = todayDate.getMonth() + 1;
-    const monthFormatted = (monthAsNumber < 10) ? "0" + monthAsNumber: monthAsNumber;
-    const dateAsNumber = todayDate.getDate();
-    const dateFormatted = (dateAsNumber < 10) ? "0" + dateAsNumber: dateAsNumber;
-    let yyyymmddFormat = `${year}-${monthFormatted}-${dateFormatted}`; 
-    return yyyymmddFormat;
-}
 
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
-
-    let todayDate = getFormattedDate();
-    fetch(`http://localhost:3000/apod/${todayDate}`)
-        .then(res => res.json())
-        .then(apod => {
-            updateStore(store, { apod });
-        })
-}
